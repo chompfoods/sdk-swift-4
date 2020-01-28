ba5be4a716846edabce9086f9533c182b599e622
petermerrill <petejmerrill@gmail.com>
2020-01-28 14:08:56 +0000

    Generated code by SwaggerHub

--- SwaggerClient/Classes/Swaggers/Models/IngredientObjectItems.swift
+++ SwaggerClient/Classes/Swaggers/Models/IngredientObjectItems.swift
@@ -33,13 +33,13 @@ public struct IngredientObjectItems: Codable {
     /** Common name associated with this item. These generally clarify what the item is (e.g. when the brand name is \&quot;BRAND&#x27;s Spicy Enchilada\&quot; the common name may be \&quot;Chicken enchilada\&quot;) */
     public var commonName: String?
 
-    /** Comments on any unusual aspects of this item. Examples might include unusual aspects of the food overall. */
+    /** Comments on any unusual aspects of this item. Examples might include unusual aspects of the food overall */
     public var footnote: String?
 
-    /** The original search term that found this food item. */
+    /** The original search term that found this food item */
     public var searchTerm: String?
 
-    /** A value that represents how similar the name of this food item is to the original search term. */
+    /** A value that represents how similar the name of this food item is to the original search term. The lower the value the closer this item&#x27;s name is to the original search term. */
     public var score: String?
     public init(name: String? = nil, categories: [String]? = nil, nutrients: [IngredientObjectNutrients]? = nil, calorieConversionFactor: IngredientObjectCalorieConversionFactor? = nil, proteinConversionFactor: BigDecimal? = nil, components: [IngredientObjectComponents]? = nil, portions: [IngredientObjectPortions]? = nil, commonName: String? = nil, footnote: String? = nil, searchTerm: String? = nil, score: String? = nil) { 
         self.name = name
